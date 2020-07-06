40b4654419e0cda55394e4f7f622d088b3fc8cdf
Juanfernandor <66078377+Juanfernandor@users.noreply.github.com>
2020-07-05 21:55:50 -0500

    Se suben clases de Parqueadero
    Se suben clases de Parqueadero

---
+++ src/Parqueadero/VehiculoOficial.java
@@ -0,0 +1,39 @@
+package Parqueadero;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class VehiculoOficial extends Vehiculo {
+	ArrayList<Estancia> estancias = new ArrayList<Estancia>();
+	int tiempoAcumulado = 0;
+
+	public VehiculoOficial(String matricula) {
+		super(matricula);
+	}
+
+	@Override
+	public void finEstancia() {
+		this.estancias.get(estancias.size()-1).horaSalida = obtenerHoraActual();
+		for (Estancia est : estancias) {
+			tiempoAcumulado += ChronoUnit.MINUTES.between(est.horaEntrada, est.horaSalida);
+		}
+	}
+
+	@Override
+	public void comienzaMes() {
+		estancias.clear();
+	}
+
+	public ArrayList<Estancia> getEstancias() {
+		return estancias;
+	}
+
+	public static LocalDateTime obtenerHoraActual() {
+		LocalDateTime horaActual = LocalDateTime.now();
+		return horaActual;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+}
